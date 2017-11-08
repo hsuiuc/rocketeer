c323199ffb9f8e442217d03a00338320e36e4ca0
haosunuiuc <sunhao19891113@hotmail.com>
2017-11-07 20:13:58 -0600

    ajax test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'abcde12345',
+    database : 'yelp_db'
+});
+connection.connect();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+    const state = req.query.state;
+    const city = req.query.city;
+    const neighbourhood = req.query.neighbourhood;
+
+    connection.query("select * from business where state = ? AND city = ? limit 5", [state, city], function (error, results, fields) {
+        if (error) {
+            throw error;
+        } else {
+            //console.log("send response : " + JSON.stringify(results));
+            res.status(200).send(JSON.stringify(results));
+        }
+    });
 });
 
 module.exports = router;
