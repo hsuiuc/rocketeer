b7029f352d9e79369b463a966b3e8fcacd8622d7
haosunuiuc <sunhao19891113@hotmail.com>
2017-11-15 19:45:46 -0600

    deploy vm

--- routes/users.js
+++ routes/users.js
@@ -37,8 +37,8 @@ router.post('/', function(req, res, next) {
             //     "longitude) VALUES ?";
             let createSql = "INSERT INTO business (id, name) VALUES ?";
             // [id, name, neighborhood, address, city, state, postal_code, lat, long]
-            let value = ['aaa', 'test'];
-            connection.query(createSql, value, function (error) {
+            let value = ['aaa, test'];
+            connection.query(createSql, [value], function (error) {
                 if (error) {
                     res.sendStatus(500);
                     throw error;
