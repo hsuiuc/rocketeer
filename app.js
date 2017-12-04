d9b5e763041350ed1221e16f5432e10cfaab1d2e
haosunuiuc <sunhao19891113@hotmail.com>
2017-12-03 20:08:44 -0600

    new endpoints

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const discover = require('./routes/discover');
+const open = require('./routes/open');
+const succeed = require('./routes/succeed');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,11 +27,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api/discover', discover);
+app.use('/api/open', open);
+app.use('/api/succeed', succeed);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
+    const err = new Error('Not Found');
+    err.status = 404;
   next(err);
 });
 
