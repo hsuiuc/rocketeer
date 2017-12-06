a42c39ac3d88b8dfb66a9b43f8a51e89a1b5c11a
haosunuiuc <sunhao19891113@hotmail.com>
2017-12-05 21:05:22 -0600

    succeed add response

--- routes/succeed.js
+++ routes/succeed.js
@@ -72,7 +72,7 @@ router.get('/', function(req, res, next) {
     //
     //     }
     // });
-    const py = spawn('python', [process.cwd() + '/resource/rocketeer.py', state, city, category, input + '1100']);
+    const py = spawn('python', [process.cwd() + '/resource/rocketeer.py', state, city, category, '111111001100']);
     console.log(process.cwd());
     let response = "";
     py.stdout.on('data', (data) => {
