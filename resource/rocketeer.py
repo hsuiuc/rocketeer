79ab3d710f094a68937a4c29a2ccc8e748074eea
haosunuiuc <sunhao19891113@hotmail.com>
2017-12-05 22:50:49 -0600

    succeed add response

--- resource/rocketeer.py
+++ resource/rocketeer.py
@@ -1,13 +1,13 @@
+import sys
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import ShuffleSplit,cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-import sys
+#from sklearn.model_selection import ShuffleSplit,cross_val_score
+#from sklearn.ensemble import RandomForestClassifier
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-from gensim import corpora, models
 import gensim
+from gensim import corpora, models
 import mysql.connector
 
 print("state")
