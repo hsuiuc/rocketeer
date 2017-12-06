4bb43c4ee2eb2cb4bbf664dbb4372ed4bf14f89a
haosunuiuc <sunhao19891113@hotmail.com>
2017-12-05 23:51:00 -0600

    succeed add response

--- routes/succeed.js
+++ routes/succeed.js
@@ -72,10 +72,8 @@ router.get('/', function(req, res, next) {
     //
     //     }
     // });
-    const py = spawn('python3.4', ['/home/shared/rocketeer/resource/rocketeer.py', 'IL', 'Champaign', 'Thai', '11000011']);
-    //const py = spawn('python', [process.cwd() + '/resource/test.py', 'IL', 'Champaign', 'Thai', '110000111001']);
-
-
+    const py = spawn('python3.4', ['/home/shared/rocketeer/resource/rocketeer.py', state, city, category, input]);
+    
     console.log(process.cwd());
     let response = "";
     console.log(`Spawned child pid: ${py.pid}`);
